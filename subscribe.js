17e63abc8ede4a6d202878aa73979d6fcdbaf3d4
Jared Bangs <jaredbangs@gmail.com>
2019-05-02 20:27:33 -0700

    Fix bug in subscribe

--- subscribe.js
+++ subscribe.js
@@ -4,7 +4,7 @@ var addSubscription = require('./actions/addSubscription');
 
 var rssUrl = process.argv[2];
 
-addSubscription(rssUrl, function (err) {
+addSubscription(rssUrl, {}, function (err) {
 
 	if (err) {
 		console.error(err);
