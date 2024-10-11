11f9a63faeb0257d0036525030b271117a425258
Jared Bangs <jaredbangs@gmail.com>
2024-10-11 16:55:13 -0700

    dep updates

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.mocha,
+            ...globals.node,
+        },
+
+        ecmaVersion: 8,
+        sourceType: "commonjs",
+    },
+
+    rules: {
+        "no-console": 0,
+        "no-useless-escape": 0,
+    },
+}, {
+    files: ["migrations/**/*.js"],
+
+    languageOptions: {
+        globals: {},
+    },
+
+    rules: {
+        "no-unused-vars": 0,
+    },
+}, {
+    files: ["models/**/*.js"],
+
+    languageOptions: {
+        globals: {},
+    },
+
+    rules: {
+        "no-unused-vars": 0,
+    },
+}];
